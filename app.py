9ce8c30eef33bb8f414379164b0b2699aebe55fd
goeha <mina1379@kakao.com>
2026-01-04 20:21:19 +0900

    ㅇㅇ

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 import os
-from flask import Flask, render_template, jsonify, make_response
+from flask import Flask, render_template, jsonify
+import requests
 import random
-import time
 
 template_dir = os.path.abspath('templates')
 app = Flask(__name__, template_folder=template_dir)
-
-# bible.txt 읽기
-def load_bible():
-    if os.path.exists('bible.txt'):
-        with open('bible.txt', 'r', encoding='utf-8') as f:
-            return [line.strip() for line in f.readlines() if line.strip()]
-    return ["항상 기뻐하라 - 데살로니가전서 5:16"]
-
-BIBLE_LINES = load_bible()
-# 절대 안 죽는 유튜브 ID (가장 대중적인 것들)
-HYMN_LIST = ["wM7id6NTo68", "BySTFpGvX9k", "68vU_W3AByE", "DCPv_I63rlo", "vAnOxl_W1fQ"]
 
 @app.route('/')
 def home():
@@ -23,19 +12,23 @@ def home():
 
 @app.route('/get_data')
 def get_data():
-    line = random.choice(BIBLE_LINES)
-    if " - " in line:
-        verse, ref = line.split(" - ")
-    else:
-        verse, ref = line, "말씀"
-    
-    # [치트키] 브라우저한테 "이거 매번 새로운 거니까 저장하지마!"라고 신호 보냄
-    response = make_response(jsonify({
-        "verse": verse,
-        "ref": ref,
-        "youtube_id": random.choice(HYMN_LIST)
-    }))
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '0'
-    return response
+    try:
+        # 처음에 잘 됐던 영어 성경 API
+        res = requests.get("https://bible-api.com/john 3:16")
+        data = res.json()
+        
+        # 유튜브는 일단 가장 안전한 거 하나만!
+        return jsonify({
+            "verse": data['text'],
+            "ref": data['reference'],
+            "youtube_id": "wM7id6NTo68" 
+        })
+    except:
+        return jsonify({
+            "verse": "For God so loved the world...",
+            "ref": "John 3:16",
+            "youtube_id": "wM7id6NTo68"
+        })
+
+if __name__ == '__main__':
+    app.run(debug=True)
